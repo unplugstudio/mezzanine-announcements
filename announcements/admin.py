303398fa081b54a33c4f9f268c6b51d51aac3ebf
Eduardo Rivas <e@jerivas.com>
2015-09-19 12:46:12 -0600

    Tweaks to admin list display.

--- announcements/admin.py
+++ announcements/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from announcements.models import Announcement
+from .models import Announcement
+
 
 class AnnouncementOptions(admin.ModelAdmin):
-    list_display = ('id', 'date_created', 'date_start', 'date_end', 'is_current', 'title',)
-    list_display_links = ('id', 'date_created', 'date_start', 'date_end',)
+    list_display = ['title', 'date_start', 'date_end', 'is_current', 'announcement_type']
 
 admin.site.register(Announcement, AnnouncementOptions)
