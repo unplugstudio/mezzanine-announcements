fb7c2fa5a9cdd8ea1e7359d4fa00f44c9e3951f7
Eduardo Rivas <e@jerivas.com>
2015-09-19 13:31:38 -0600

    Tweak list_display and date_hierarchy in the admin.

--- announcements/admin.py
+++ announcements/admin.py
@@ -4,6 +4,9 @@ from .models import Announcement
 
 
 class AnnouncementOptions(admin.ModelAdmin):
-    list_display = ['title', 'date_start', 'date_end', 'is_current', 'announcement_type']
+    list_display = ['title', 'announcement_type', 'date_start', 'date_end', 'is_active',
+                    'can_dismiss']
+    list_filter = ['announcement_type', 'can_dismiss']
+    date_hierarchy = 'date_start'
 
 admin.site.register(Announcement, AnnouncementOptions)
