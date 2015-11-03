b801288646cffefd35e0cd84fd8401568507823b
Eduardo Vela <eduardo3vela@gmail.com>
2015-11-02 22:34:46 -0600

    Use js.cookie to add announcement dismiss cookie.

--- announcements/static/js/announcements.js
+++ announcements/static/js/announcements.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-    // http://www.w3schools.com/JS/js_cookies.asp
-    function setCookie(c_name, value, expiredays) {
-        var exdate = new Date();
-        exdate.setDate(exdate.getDate() + expiredays);
-        document.cookie=c_name + "=" + escape(value) +
-            ((expiredays === null) ? "" : ";expires=" + exdate.toUTCString());
-    }
-
     $(".close-announcement").on("click", function() {
         $this = $(this);
         var announcementId = $this.data("announcementId");
@@ -14,6 +6,6 @@ $(document).ready(function() {
         if ($this.attr("data-expire-days")) {
             expiredays = $this.data("expireDays");
         }
-        setCookie("announcements_dismiss", announcementId, expiredays);
+        Cookies.set("announcements_dismiss", announcementId, {expires: expiredays});
     });
 });
