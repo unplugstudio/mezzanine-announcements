7fffecf763405201af210bffc36e41a9d8008b66
Eduardo Vela <eduardo3vela@gmail.com>
2015-11-13 19:16:23 -0600

    Wrap close announcement function outside the on click event.

--- announcements/static/js/announcements.js
+++ announcements/static/js/announcements.js
@@ -1,26 +1,28 @@
+function closeAnnouncement(element) {
+    $this = $(element);
+    var cname = "announcements_dismiss";
+    var announcementId = ($this.data("announcementId")).toString();
+    var expiredays = 365
+    var cvalue = Cookies.get(cname);
+
+    if (cvalue) {
+        cvalue = cvalue.split("_");
+        if (cvalue.indexOf(announcementId) < 0) {
+            cvalue.push(announcementId)
+        }
+        cvalue = cvalue.join("_");
+    } else {
+        cvalue = announcementId;
+    }
+    if ($this.attr("data-expire-days")) {
+        expiredays = $this.data("expireDays");
+    }
+    Cookies.set(cname, cvalue, {expires: expiredays});
+
+    // Add fadeout transition to dismissable announcements
+    $this.closest(".announcement").fadeOut();
+}
+
 $(document).ready(function() {
-    $(".close-announcement").on("click", function() {
-        $this = $(this);
-        var cname = "announcements_dismiss";
-        var announcementId = ($this.data("announcementId")).toString();
-        var expiredays = 365
-        var cvalue = Cookies.get(cname);
-
-        if (cvalue) {
-            cvalue = cvalue.split("_");
-            if (cvalue.indexOf(announcementId) < 0) {
-                cvalue.push(announcementId)
-            }
-            cvalue = cvalue.join("_");
-        } else {
-            cvalue = announcementId;
-        }
-        if ($this.attr("data-expire-days")) {
-            expiredays = $this.data("expireDays");
-        }
-        Cookies.set(cname, cvalue, {expires: expiredays});
-
-        // Add fadeout transition to dismissable announcements
-        $this.closest(".announcement").fadeOut();
-    });
+    $(".close-announcement").on("click", function() {closeAnnouncement(this);});
 });
