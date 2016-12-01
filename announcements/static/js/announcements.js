98f8f0bdf4dde57158c45af0326bdefe26724bcb
Eduardo Rivas <e@jerivas.com>
2016-11-30 23:17:27 -0600

    Tweak JS to extract all data attrs from .announcement.

--- announcements/static/js/announcements.js
+++ announcements/static/js/announcements.js
@@ -1,38 +1,42 @@
-function closeAnnouncement(element) {
-    $this = $(element);
-    var cname = "announcements_dismiss";
-    var announcementId = ($this.data("announcementId")).toString();
-    var expiredays = 365
-    var cvalue = Cookies.get(cname);
+// Close the parent .announcement when a button is clicked
+// And store the closed announcement ID in a cookie
+// so we don't show it again to the same user
+function close(event) {
+    var $announcement = $(event.target).closest(".announcement");
+    var announcementId = $announcement.data("announcementId").toString();
+    var expiredays = $announcement.data("expireDays") || 365;
+    var cookieName = "announcements_dismiss";
+    var cookieValue = Cookies.get(cookieName);
 
-    if (cvalue) {
-        cvalue = cvalue.split("_");
-        if (cvalue.indexOf(announcementId) < 0) {
-            cvalue.push(announcementId)
+    // If the announcement cookie is present, parse it as an array
+    if (cookieValue) {
+        cookieValue = cookieValue.split("_");
+        if (cookieValue.indexOf(announcementId) < 0) {
+            cookieValue.push(announcementId);
         }
-        cvalue = cvalue.join("_");
+        cookieValue = cookieValue.join("_");
+    // Else, just add the current announcement id as the first element
     } else {
-        cvalue = announcementId;
+        cookieValue = announcementId;
     }
-    if ($this.attr("data-expire-days")) {
-        expiredays = $this.data("expireDays");
-    }
-    Cookies.set(cname, cvalue, {expires: expiredays});
 
-    // Add fadeout transition to dismissable announcements
-    $this.closest(".announcement").fadeOut();
+    // Store the cookie with the expiration information
+    Cookies.set(cookieName, cookieValue, {expires: expiredays});
+
+    // Close the announcement with a fade
+    $announcement.fadeOut();
 }
 
 $(document).ready(function() {
+    // Show announcements (include delay as needed)
     $(".announcement").each(function() {
-        $this = $(this);
-        var delay_time = $this.data("appearanceDelay");
-        var announcement_id = $this.data("announcementId");
+        var $this = $(this);
+        var delay_time = $this.data("appearanceDelay") || 0;
         setTimeout(function(){
-            $(".announcement[data-announcement-id="
-                + announcement_id + "]").fadeIn("slow");
+            $this.fadeIn();
         }, delay_time);
     });
 
-    $(".close-announcement").on("click", function() {closeAnnouncement(this);});
+    // Bind the close buttons on each announcement
+    $(".close-announcement").on("click", close);
 });
