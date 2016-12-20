d5a0498dd513d83bad7095eb24d1e51f31722631
Eduardo Vela <eduardo3vela@gmail.com>
2016-12-20 19:55:57 +0100

    Create template filter for generating an iframe from an URL.

--- announcements/templatetags/announcements_tags.py
+++ announcements/templatetags/announcements_tags.py
@@ -1,4 +1,4 @@
-# calling this file announcements.py makes the namespace clash with the app :(
+import urlparse
 
 from announcements import defaults
 from django.template import Library
@@ -14,3 +14,34 @@ def dismiss_js_link(a):
         defaults.ANNOUNCEMENTS_COOKIE_NAME,
         a.pk
     ))
+
+
+@register.filter
+def generate_video_iframe(url):
+    """
+    Filter used to construct an iframe element from an URL of a video, either
+    from YouTube or Vimeo.
+    """
+    hostname = ""
+    video_type = ""
+    video_id = None
+
+    url_data = urlparse.urlparse(url)
+
+    if url_data.hostname in ("www.vimeo.com", "vimeo.com"):
+        hostname = "player.vimeo.com"
+        video_type = "video"
+        video_id = url_data.path[1:]
+
+    if url_data.hostname in ("www.youtube.com", "youtube.com", "youtu.be"):
+        hostname = "www.youtube.com"
+        video_type = "embed"
+        if url_data.hostname == "youtu.be":
+            video_id = url_data.path[1:]
+        if video_id is None:
+            query = urlparse.parse_qs(url_data.query)
+            video_id = query["v"][0]
+
+    return ("<iframe src='https://{hostname}/{type}/{id}?autoplay=1' frameborder='0'"
+            "allowfullscreen></iframe>"
+            .format(hostname=hostname, type=video_type, id=video_id))
