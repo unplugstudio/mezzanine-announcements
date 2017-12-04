2373ca8ad8bfbf650f6b78e913e24ea9de606966
Eduardo Rivas <e@jerivas.com>
2017-12-04 12:19:42 -0600

    Admin: Minor fixes.

--- announcements/admin.py
+++ announcements/admin.py
@@ -6,19 +6,18 @@ from .models import Announcement
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
-    exclude = ['template']
-    list_display = ['title', 'announcement_type', 'date_start', 'date_end', 'is_active',
-                    'can_dismiss']
-    list_filter = ['announcement_type', 'can_dismiss']
-    date_hierarchy = 'date_start'
+    exclude = ["template"]
+    list_display = ["title", "announcement_type", "date_start", "date_end", "is_active",
+                    "can_dismiss"]
+    list_filter = ["announcement_type", "can_dismiss"]
+    date_hierarchy = "date_start"
 
     def get_form(self, request, obj=None, **kwargs):
         if not settings.ANNOUNCEMENTS_VIDEO_ENABLED:
             if self.exclude is not None:
                 self.exclude.append("video_link")
             else:
-                self.exculde = ["video_link"]
-        print self.exclude
+                self.exclude = ["video_link"]
         form = super(AnnouncementAdmin, self).get_form(request, obj, **kwargs)
         return form
 
